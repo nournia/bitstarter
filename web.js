e5f8d5605a967a3f56fe5fe10ed3dc6478b1902c
Alireza Nourian <alireza.nournia@gmail.com>
2013-07-05 21:58:24 +0430

    add index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+	response.send(index);
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
